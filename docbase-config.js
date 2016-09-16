189ba1bfb6f97b697c6ad7e471fc98be2c082f12
Farhan Chauhan <farhan687@gmail.com>
2016-09-16 19:30:46 +0530

    Update docbase-config.js

--- docbase-config.js
+++ docbase-config.js
@@ -25,60 +25,8 @@ var docbaseConfig = {
       "label": "Getting Started",
       "files": [
         {
-          "name": "start",
+          "name": "guide",
           "label": "Quick Start"
-        },
-        {
-          "name": "configure",
-          "label": "Configuration Options"
-        }
-      ]
-    },
-    {
-      "name": "features",
-      "label": "Features",
-      "files": [
-        {
-          "name": "search",
-          "label": "Search"
-        },
-        {
-          "name": "colors",
-          "label": "Colorful"
-        },
-        {
-          "name": "gh-pages",
-          "label": "Deploy to Github"
-        },
-        {
-          "name": "versions",
-          "label": "Versatile Navigation"
-        }
-      ]
-    },
-    {
-      "name": "layout",
-      "label": "Layout",
-      "files": [
-        {
-          "name": "navigation",
-          "label": "Site Navigation"
-        },
-        {
-          "name": "3col",
-          "label": "Three columns"
-        }
-      ]
-    }
-  ],
-  "v2": [
-    {
-      "name": "changelog",
-      "label": "Changes",
-      "files": [
-        {
-          "name": "changelog",
-          "label": "Changelog"
         }
       ]
     }
