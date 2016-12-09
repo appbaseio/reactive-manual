2ca5c2fc570b6668e86e297a343739914630f6fd
Siddharth Kothari <sids.aquarius@gmail.com>
2016-12-09 04:51:36 -0600

    Update docbase-config.js

--- docbase-config.js
+++ docbase-config.js
@@ -1,76 +1,89 @@
-var docbaseConfig = {
-  "method": "github",
-  "generic": {
-    "baseurl": "",
-    "path": ""
-  },
-  "file": {
-    "path": ""
-  },
-  "github": {
-    "user": "appbaseio",
-    "repo": "reactive-maps-docs",
-    "path": "docs",
-    "branch": "master",
-    "access_token": "OTAwNDU5MTk4M2NlYzViYzQxNzFlN2E3Yjk1MWQ2ZjhkODRjMTg5Yg=="
-  },
-  "indexHtml": "./html/main.html",
-  "flatdocHtml": "./bower_components/docbase/html/flatdoc.html",
-  "default_version": "",
-  "manual_override": false,
-  "versions" : {
-  "v1": [
-    {
-      "name": "getting-started",
-      "label": "Getting Started",
-      "files": [
-        {
-          "name": "start",
-          "label": "Quick Start"
-        },
-        {
-          "name": "AppbaseList",
-          "label": "AppbaseList Sensor"
-        },
-        {
-          "name": "AppbaseSearch",
-          "label": "AppbaseSearch Sensor"
-        },
-        {
-          "name": "AppbaseSlider",
-          "label": "AppbaseSlider Sensor"
-        },
-        {
-          "name": "AppbaseMap",
-          "label": "AppbaseMap Sensor"
-        },
-        {
-          "name": "Dependency",
-          "label": "Dependency"
-        }
+var docbaseConfig ={
+   "method":"github",
+   "generic":{
+      "baseurl":"",
+      "path":""
+   },
+   "file":{
+      "path":""
+   },
+   "github":{
+      "user":"appbaseio",
+      "repo":"reactive-maps-docs",
+      "path":"docs",
+      "branch":"master",
+      "access_token":"OTAwNDU5MTk4M2NlYzViYzQxNzFlN2E3Yjk1MWQ2ZjhkODRjMTg5Yg=="
+   },
+   "indexHtml":"./html/main.html",
+   "flatdocHtml":"./bower_components/docbase/html/flatdoc.html",
+   "default_version":"",
+   "manual_override":false,
+   "versions":{
+      "v1":[
+         {
+            "name":"components",
+            "label":"Components",
+            "files":[
+               {
+                  "name":"SingleList",
+                  "label":"SingleList"
+               },
+               {
+                  "name":"MultiSelect",
+                  "label":"MultiSelect"
+               }
+            ]
+         },
+         {
+            "name":"getting-started",
+            "label":"Getting Started",
+            "files":[
+               {
+                  "name":"start",
+                  "label":"Quick Start"
+               },
+               {
+                  "name":"AppbaseList",
+                  "label":"AppbaseList Sensor"
+               },
+               {
+                  "name":"AppbaseSearch",
+                  "label":"AppbaseSearch Sensor"
+               },
+               {
+                  "name":"AppbaseSlider",
+                  "label":"AppbaseSlider Sensor"
+               },
+               {
+                  "name":"AppbaseMap",
+                  "label":"AppbaseMap Sensor"
+               },
+               {
+                  "name":"Dependency",
+                  "label":"Dependency"
+               }
+            ]
+         },
+         {
+            "name":"mapping",
+            "label":"mapping",
+            "files":[
+               {
+                  "name":"mapping",
+                  "label":"Mapping"
+               }
+            ]
+         },
+         {
+            "name":"contribution",
+            "label":"Contribution",
+            "files":[
+               {
+                  "name":"contribution",
+                  "label":"contribution"
+               }
+            ]
+         }
       ]
-    },
-    {
-      "name": "mapping",
-      "label": "mapping",
-      "files": [
-        {
-          "name": "mapping",
-          "label": "Mapping"
-        }
-      ]
-    },
-    {
-      "name": "contribution",
-      "label": "Contribution",
-      "files": [
-        {
-          "name": "contribution",
-          "label": "contribution"
-        }
-      ]
-    }
-  ]
-},
-  "publish": "github"
+   }
 }
