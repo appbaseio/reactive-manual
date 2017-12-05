cbd0c5d464209822e9756370c316010fc1c49363
Divyanshu Maithani <div.blackcat@gmail.com>
2017-12-05 17:14:03 +0530

    :art: Update gatsby code highlight styles

--- src/prism-styles.js
+++ src/prism-styles.js
@@ -5,7 +5,7 @@ const prismColors = {
   char: '#D8DEE9',
   comment: '#999999',
   keyword: '#c5a5c5',
-  lineHighlight: '#14161a',
+  lineHighlight: '#222731',
   primitive: '#5a9bcf',
   string: '#8dc891',
   variable: '#d7deea',
