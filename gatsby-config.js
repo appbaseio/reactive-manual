1f936a64dd535d998a86e045684103db12f552dd
kuldeep <kuldepsaxena155@gmail.com>
2019-04-22 19:26:15 +0530

    feat: change to v2

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: '/reactive-manual',
+  pathPrefix: '/reactive-manual/v2',
   siteMetadata: {
     title:
       'Reactive Manual: Data-driven components for building Maps and Search UIs',
